45e3be43c89a8e58e37d1c671edf38a2bae1acf4
TransonQ <quanscheng@qq.com>
2025-09-19 16:26:00 +0800

    eslint & prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,6 +18,30 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off', // 放开对 any 的限制
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'none', // 忽略函数参数
+          caughtErrors: 'none', // 忽略捕获的错误
+          destructuredArrayIgnorePattern: '^_', // 忽略解构数组中以下划线开头的变量
+          varsIgnorePattern: '^_', // 忽略以下划线开头的变量
+        },
+      ],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true, // 允许短路逻辑表达式
+          allowTernary: true, // 可选：允许三元表达式
+        },
+      ],
     },
   },
 ])
--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,5 @@
+
 export function App() {
+
   return <>vite-react-vscode-template</>
 }
